4a2021909d3f1545cd2b71443da4121c1520c40d
Michelle Jeppesen <michellejeppesen@gmail.com>
2022-01-22 10:28:23 -0700

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureELement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureELement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "5d8072f5eec44d31c43b75c5fe5b9450";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
