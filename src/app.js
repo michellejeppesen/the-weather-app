b6de99e9cd12dbecc697c7744a3ccf0a461b12b4
Michelle Jeppesen <michellejeppesen@gmail.com>
2022-01-22 10:48:50 -0700

    Remove console.log

--- src/app.js
+++ src/app.js
@@ -25,7 +25,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureELement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
