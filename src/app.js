bf6d15410a760282193061cdcd8fd4b7818eb0ad
Michelle Jeppesen <michellejeppesen@gmail.com>
2022-01-22 10:48:01 -0700

    Update time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+//timestamp is the number of milliseconds that has happened since 1970 - that's how dates in javascript work
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  //this is to help incase the minutes or hour is just 5
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  //without doing the array above and then getting the number of the array it wil just give a number of the day of the week that it is
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureELement = document.querySelector("#temperature");
@@ -5,11 +31,13 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureELement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "5d8072f5eec44d31c43b75c5fe5b9450";
