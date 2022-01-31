70340e9d9633b9790e5067cc86e9a0f67dcb62eb
Michelle Jeppesen <michellejeppesen@gmail.com>
2022-01-31 08:56:22 -0700

    Added in forecast - html

--- src/app.js
+++ src/app.js
@@ -24,6 +24,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Sun", "Mon", "Tue", "Wed"];
+  
+  let forecastHTML = `<div class="row">`;
+  
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+                <div class="col-2">
+                  <div class="weather-forecast-date">${day}</div>
+                  <img
+                    src="http://openweathermap.org/img/wn/04n@2x.png"
+                    alt=""
+                    width="42"
+                  />
+                  <div class="weather-forecast-temperature">
+                    <span class="weather-forecast-temperature-max"> 18° </span>
+                    <span class="weather-forecast-temperature-min"> 12° </span>
+                  </div>
+                </div>
+              
+            `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureELement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -95,3 +125,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
